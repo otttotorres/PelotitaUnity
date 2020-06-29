216c4842cb8371b37352b0774ed72b765c908f1c
Otto Torres <otso_pa@hotmail.com>
2020-06-29 18:42:32 +0200

    Version 1.0

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacle : MonoBehaviour
+{
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Text score;
+
+    public Rigidbody rb;
+
+    public float forwardForce = 1000;
+
+    public float sideForce = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Viva Chavez");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+
+        score.text = transform.position.z.ToString("0");
+
+    }
+}
